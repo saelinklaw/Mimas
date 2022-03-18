71781ca6ade125dd82cdffc5e8f06c0b3ec74427
saelinklaw <72177966+saelinklaw@users.noreply.github.com>
2022-03-18 13:30:54 +0800

    2022031

--- server.js
+++ server.js
@@ -79,7 +79,7 @@
     res.writeHead(200, {
       'Content-Type': 'text/plain'
     });
-    return res.end("Panasonic");
+    return res.end("Scattershot proxmoxve-v@protonmail.com");
   });
 
   wss = new WebSocketServer({
